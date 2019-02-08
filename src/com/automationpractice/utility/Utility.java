f755e518d975c74835ede886a85740868fccb6b6
Iza <iza.wojcik.iw@gmail.com>
2019-02-08 23:26:01 +0100

    Added config.properties and Utility.class

---
+++ src/com/automationpractice/utility/Utility.java
@@ -0,0 +1,16 @@
+package com.automationpractice.utility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Utility {
+
+	public static String fetchBrowserName(String key) throws IOException {
+		FileInputStream file = new FileInputStream("./Config/config.properties");
+		Properties property = new Properties();
+		property.load(file);
+		return property.get(key).toString();
+	}
+}
