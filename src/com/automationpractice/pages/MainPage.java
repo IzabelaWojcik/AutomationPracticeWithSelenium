7f1cf67af2505a3cb6b9d351954fc31b1af18192
Iza <iza.wojcik.iw@gmail.com>
2019-02-21 14:04:50 +0100

    code refactoring

--- src/com/automationpractice/pages/MainPage.java
+++ src/com/automationpractice/pages/MainPage.java
@@ -3,7 +3,6 @@ package com.automationpractice.pages;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -24,11 +23,10 @@ public class MainPage {
 	public void mouseOverFirstItemToBuy() throws IOException, InterruptedException {
 		WebElement element = driver.findElement(By.xpath(Utility.fetchLocatorValue("firstItemToBuy_li_xpath")));
 		
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-		Thread.sleep(500); 
+		Utility.scroolIntoView(driver, element); 
 		
 		Actions act = new Actions(driver);
-		act.moveToElement(driver.findElement(By.xpath(Utility.fetchLocatorValue("firstItemToBuy_li_xpath")))).perform();
+		act.moveToElement(element).perform();
 	}
 	
 }
--- src/com/automationpractice/utility/Utility.java
+++ src/com/automationpractice/utility/Utility.java
@@ -4,6 +4,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class Utility {
 
 	public static String fetchPropertyValue(String key) throws IOException {
@@ -18,5 +22,10 @@ public class Utility {
 		Properties property = new Properties();
 		property.load(file);
 		return property.get(key).toString();
+	}
+	
+	public static void scroolIntoView(WebDriver driver, WebElement element) throws InterruptedException {
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+		Thread.sleep(500); 
 	}
 }
