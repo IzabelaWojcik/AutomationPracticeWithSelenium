3e73acaa1c5dac964b455355b7966405d9ef03a1
Iza <iza.wojcik.iw@gmail.com>
2019-02-21 14:43:51 +0100

    code refactoring

--- src/com/automationpractice/pages/MainPage.java
+++ src/com/automationpractice/pages/MainPage.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 import com.automationpractice.utility.Utility;
 
@@ -20,13 +19,12 @@ public class MainPage {
 		driver.findElement(By.className(Utility.fetchLocatorValue("login_button_className"))).click();
 	}
 	
-	public void mouseOverFirstItemToBuy() throws IOException, InterruptedException {
-		WebElement element = driver.findElement(By.xpath(Utility.fetchLocatorValue("firstItemToBuy_li_xpath")));
-		
-		Utility.scroolIntoView(driver, element); 
-		
-		Actions act = new Actions(driver);
-		act.moveToElement(element).perform();
+	public WebElement getFirstItemToBuy() throws IOException, InterruptedException {
+		return driver.findElement(By.xpath(Utility.fetchLocatorValue("firstItemToBuy_li_xpath")));
+	}
+	
+	public void clickedOnQuickViewElement() throws IOException {
+		driver.findElement(By.xpath(Utility.fetchLocatorValue("firstItemToBuyQuickView_a_xpath")));
 	}
 	
 }
--- src/com/automationpractice/testcases/MainPageTest.java
+++ src/com/automationpractice/testcases/MainPageTest.java
@@ -4,6 +4,8 @@ import static org.testng.Assert.assertTrue;
 
 import java.io.IOException;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import com.automationpractice.assertions.Compare;
@@ -25,8 +27,14 @@ public class MainPageTest extends SetUps{
 	public void moveOverElementShowsBuyingAndDetailsOptions() throws IOException, InterruptedException {
 		MainPage page = new MainPage(driver);
 		
-		page.mouseOverFirstItemToBuy();
+		WebElement element = page.getFirstItemToBuy();
+		Utility.scroolIntoView(driver, element); 
+		
+		Actions act = new Actions(driver);
+		act.moveToElement(element).perform();
 		
 	}
+	
+	
 	
 }
